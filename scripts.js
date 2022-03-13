bf12e7cec446e9cfb6123c6c1edf2f76dda0fdf1
AlanSkooter <alinasozonovich@gmail.com>
2022-03-13 10:01:38 +0300

    add

--- scripts.js
+++ scripts.js
@@ -1,13 +1,15 @@
 const button = document.getElementById('add-btn');
+const buttonDelete = document.getElementById('del-btn');
 const sum = document.getElementById('sum');
 const container = document.getElementById('container');
-const line = document.getElementById('line');
+const line1 = document.getElementById('line1');
+const line2 = document.getElementById('line2');
 const select = document.getElementById('select');
 let balance = document.getElementById('balance');
 let consumption = document.getElementById('consumption');
 
 const warning = document.createElement('div');
-line.before(warning);
+line1.before(warning);
 balance.innerHTML = `<p>Доступно средств: 0</p>`;
 consumption.innerHTML = `<p>Израсходовано: 0</p>`;
 
@@ -30,6 +32,12 @@ balance.innerHTML = `<p>Доступно средств: ${countBalance}</p>`;
 consumption.innerHTML = `<p>Израсходовано: ${countConsumption}</p>`;
 }
 
+sum.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+    button.click();
+  }
+});
+
 button.onclick = () => {
   const sumValue = +sum.value;
   const selectValue = select.value;
@@ -45,7 +53,7 @@ button.onclick = () => {
         let array = JSON.parse(localStorage.getItem('addSum')||'[]');
         array.push(addSum);
         localStorage.setItem('addSum', JSON.stringify(array));
-        sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type="checkbox" class = "visually-hidden" id = "check"> <span class="checker"></span> <span class="sumvalue"> + ${sumValue.toFixed(2)} </span></label>`;
+        location.reload();
       }
       else if (selectValue === 'outgo') {
         let addSum = {
@@ -55,7 +63,7 @@ button.onclick = () => {
         let array = JSON.parse(localStorage.getItem('addSum')||'[]');
         array.push(addSum);
         localStorage.setItem('addSum', JSON.stringify(array));
-        sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type="checkbox" class = "visually-hidden" id = "check"> <span class="checker"> </span> <span class="sumvalue"> − ${sumValue.toFixed(2)}</span></label>`;
+        location.reload();
       }
   } else {
     warning.innerHTML = `<p id = "warning"> Введите сумму! </p>`; 
@@ -66,15 +74,20 @@ button.onclick = () => {
 
 const reloaded  = () => {
     let arraySumAll = JSON.parse(localStorage.getItem('addSum'));
-    arraySumAll.forEach(elem => {
+    if (arraySumAll.length > 0) {
+      line2.style.display = "block";
+    } else {
+      line2.style.display = "none";
+    }
+    arraySumAll.forEach((elem, index) => {
     if (elem.select === 'income') {
       const sumDiv = document.createElement('div');
       container.prepend(sumDiv);
-      sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type="checkbox" class = "visually-hidden" id = "check"> <span class="checker"></span>  <span class="sumvalue"> + ${elem.value} </span></label>`;
+      sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type = "checkbox" class = "visually-hidden" id = "${index}" onchange = "delBtn();" > <span class="checker"></span>  <span class="sumvalue"> + ${elem.value} </span></label>`;
     } else if (elem.select === 'outgo') {
       const sumDiv = document.createElement('div');
       container.prepend(sumDiv);
-      sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type="checkbox" class = "visually-hidden" id = "check"> <span class="checker"></span>  <span class="sumvalue"> − ${elem.value} </span></label>`;
+      sumDiv.innerHTML = `<label class = "custom-checkbox"> <input type = "checkbox" class = "visually-hidden" id = "${index}" onchange = "delBtn();" > <span class="checker"></span>  <span class="sumvalue"> − ${elem.value} </span></label>`;
     }
   });
   let countBalance = JSON.parse(localStorage.getItem('balance'));
@@ -84,10 +97,32 @@ const reloaded  = () => {
 }
 
 window.onload = () => {
-  let loaded = sessionStorage.getItem('loaded');
-  if (loaded) {
     reloaded();
-  } else {
-    sessionStorage.setItem('loaded', true);
-  }
+}
+
+const remove = () => {
+  let checkboxes = document.querySelectorAll('.visually-hidden');
+  let array = JSON.parse(localStorage.getItem('addSum')||'[]');
+  checkboxes.forEach(elem => {
+    if (elem.checked) {
+      let index = elem.id;
+      array.splice(index, 1);
+    }
+  });
+  localStorage.setItem('addSum', JSON.stringify(array));
+  location.reload();
+  countSum();
+}
+
+buttonDelete.onclick = () => {
+  remove();
+}
+
+const delBtn = () => {
+  let checkboxes = document.querySelectorAll("input[type='checkbox']:checked");
+    if (checkboxes.length > 0) {
+      buttonDelete.style.display = "block";
+    } else {
+      buttonDelete.style.display = "none";
+    }
 }
